084e8b398b69bd374d857c95bfe354e018d9b4a7
OlyaKhromova <olya.hrmvlg@gmail.com>
2022-07-30 11:59:12 +0300

    Исправлена ршибка в скрипте для квадрата; добавлены классы "enterNumbers" и "answers"; исправлены отступы.

--- script/square.js
+++ script/square.js
@@ -5,7 +5,7 @@ var squareArea;
 function square() {
     squareSide = Number(document.getElementById("squareSide").value);
     squareArea = Math.pow(squareSide, 2);
-    squarePerimeter = squareArea * 4;
+    squarePerimeter = squareSide * 4;
 
     document.getElementById("squareArea").innerHTML = squareArea;
     document.getElementById("squarePerimetr").innerHTML = squarePerimeter;
